070b1da891949f558b062e8b42ea98ff8f4cd96b
vignolo_3@hotmail.com <vignolo_3@hotmail.com>
2022-06-06 18:32:43 +0200

    login text 2

--- components/auth/AuthForm.js
+++ components/auth/AuthForm.js
@@ -88,7 +88,7 @@ function AuthForm() {
           >
             {isLogin ? 'Create new account' : 'Login with existing account'}
           </button>
-          <h3 className="first-time">
+          <h3 className="first-time" style={{ color: '#DA353F' }}>
             If this is your first time in the App, create an account
           </h3>
 
